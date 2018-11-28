ce490d1d0c2ed62586b166f40938ae1f597f5898
Alyssa Kennedy <alyssadrandall@gmail.com>
2018-11-28 04:38:32 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,13 @@
-#write your code here
+def countdown(number)
+  number = 10
+  while number < 10
+    puts "#{number} SECOND(S)!"
+    number _= 0
+  end
+  puts "HAPPY NEW YEAR!"
+end
 
-def countdown
+def countdown_with_sleep
+  end
+  puts "HAPPY NEW YEAR!"
 end
